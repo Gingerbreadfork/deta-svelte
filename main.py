f617fefad87299f22876e8ad2f99e2e96c2251e3
Gingerbreadfork <gingerbreadfork@protonmail.com>
2021-06-11 01:53:15 +1000

    Further Simplified

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return FileResponse('svelte/public/index.html')
-
-# Place After All Other Routes
-app.mount('/', StaticFiles(directory="svelte/public/"), name="static")
+app.mount('', StaticFiles(directory="svelte/public/", html=True), name="static")
